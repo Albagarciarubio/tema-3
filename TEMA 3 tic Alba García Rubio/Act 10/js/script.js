7b5d7f4cfdf2015873057042fd3e0b8c4074d409
Albagarciarubio <151036220+Albagarciarubio@users.noreply.github.com>
2024-02-23 20:54:54 +0100

    Add files via upload

---
+++ TEMA 3 tic Alba García Rubio/Act 10/js/script.js
@@ -0,0 +1,13 @@
+function Calificar() {
+    let alumnos = ["Julia", "Alba G", "Rosalin", "Alvaro"];
+    let nota = [];
+        nota[1]=parseInt(prompt("¿Cuál es la nota de " + alumnos[0] + "?"));
+
+        nota[2]=parseInt(prompt("¿Cuál es la nota de " + alumnos[1] + "?"));
+
+        nota[3]=parseInt(prompt("¿Cuál es la nota de " + alumnos[2] + "?"));
+
+        nota[4]=parseInt(prompt("¿Cuál es la nota de " + alumnos[3] + "?"));
+        
+        document.getElementById("solucion").innerHTML = "Las notas son" + "<br>Julia: "  nota[1] + "<br>Alba G:" nota[2] + "<br>Rosalin:" + nota[3] + "<br>Alvaro" + nota[4];
+}
